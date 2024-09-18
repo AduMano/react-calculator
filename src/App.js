acafed2d3b4b314f578dfe7c9cfef11c2f2ae4e1
AduMano <aldwin.dylan@gmail.com>
2024-09-18 14:21:22 +0800

    Initiated Calculator Design, Not extracted yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/calculator.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calcWrapper">
+      <div className="calcNavMenu">
+        {/* 2 of these three list is interactable, by pressing the button to change the modes. */}
+        {/* Those button has their own state if they are selected or not */}
+        <ul>
+          <li>
+            <h3>CALCU</h3>
+          </li>
+          <li>
+            <button>BASIC</button>
+          </li>
+          <li>
+            <button>STANDARD</button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="calcHeader">
+        {/* The Heading's position is absolute to ignore the flow rule */}
+        <button>☰</button>
+        {/* Then the Menu Button will be relative to the parent's position */}
+        <h2>BASIC</h2>
+      </div>
+
+      <div className="calcBody">
+        {/* This depends on which mode is selected */}
+        <div className="calcBasicMode"></div>
+        <div className="calcStandardMode"></div>
+      </div>
     </div>
   );
 }
